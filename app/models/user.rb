efb2b40ca747588c89b52f6bd9603e55cbe8822c
lin <niki.linzhang@gmail.com>
2018-03-20 16:46:43 +1100

    WIP

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates(:name, presence: true, length: { maximum: 50 })
+  validates(:mobile, presence: true, length: { maximum: 50 })
+  has_secure_password
+  validates(:password, presence: true, length: { minimum: 6, maximum: 50 })
+end
